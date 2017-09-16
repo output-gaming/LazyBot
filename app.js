d1136b43a029166490ddd371d361252e520952a3
Luke Alford <luke@lukealford.com>
2017-09-17 04:27:29 +1000

    Added battlemetrics support
    currently only !bans works, this will return the current count of active, inactive & total

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const botSettings = require("./botsettings.json");
 const Discord = require("discord.js");
 
+var axios = require("axios");
+
+axios.defaults.baseURL =  'https://api.battlemetrics.com';
+axios.defaults.headers.common['Authorization'] = "Bearer privateyo";
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+
 const bot = new Discord.Client({disableEveryone: true});
 
 // Here we load the config.json file that contains our token and our prefix values.
@@ -10,11 +17,28 @@ const config = require("./botsettings.json");
 
 bot.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`);
+  console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds. `);
   // Example of changing the bot's playing game to something useful. `bot.user` is what the
   // docs refer to as the "botUser".
+    bot.user.setGame(`on ${bot.guilds.size} servers`);
 });
 
+bot.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
+bot.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  bot.user.setGame(`on ${bot.guilds.size} servers`);
+});
+
+
+bot.on('guildMemberAdd', member => {
+   member.send("Welcome to the Team Revolt discord!, use !help for more info and rules.");
+});
 
 bot.on("message", async message => {
   // This event will run on every single message received, from any channel or DM.
@@ -53,8 +77,57 @@ bot.on("message", async message => {
     message.channel.send(sayMessage);
   }
 
+  if(command === "kick") {
+    // This command must be limited to mods and admins. In this example we just hardcode the role names.
+    // Please read on Array.some() to understand this bit:
+    // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
+    if(!message.member.roles.some(r=>["Team Revolt Admin", "Team Revolt Staff" , "Game Server Admin"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+
+    // Let's first check if we have a member and if we can kick them!
+    // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.kickable)
+      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+
+    // slice(1) removes the first part, which here should be the user mention!
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("Please indicate a reason for the kick!");
+
+    // Now, time for a swift kick in the nuts!
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+
+  }
+
+  if(command === "ban") {
+    // Most of this command is identical to kick, except that here we'll only let admins do it.
+    // In the real world mods could ban too, but this is just an example, right? ;)
+    if(!message.member.roles.some(r=>["Team Revolt Admin"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.bannable)
+      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("Please indicate a reason for the ban!");
+
+    await member.ban(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+  }
 
   if(command === "purge") {
+    if(!message.member.roles.some(r=>["Team Revolt Admin"].includes(r.name)) )
+     return message.reply("Sorry, you don't have permissions to use this!");
     // This command removes all messages from all users in the channel, up to 100.
     // get the delete count, as an actual number.
     const deleteCount = parseInt(args[0], 10);
@@ -70,16 +143,68 @@ bot.on("message", async message => {
   }
   if (command === 'help') {
 
-      let embed = new Discord.RichEmbed()
-          .setAuthor("Team Revolt Bot")
-          .setDescription("Below you will find some basic help.")
-          .setColor("#3f542f")
-          .addField("Join our Forum", 'All are welcome, https://www.teamrevolt.org/forums/')
-          .addField("Team Speak", 'ts3.teamrevolt.org')
-          .addField("Profile Restore", '!profile will tell let you know how to restore your KoTH profile, do remember its easier to keep backups.');
+    message.author.send({embed:{
+        color: 3447003,
+        author: {
+          name: bot.user.username,
+          icon_url: bot.user.avatarURL
+        },
+        title: "Hi, I'm LazyBot. Let me try and help you",
+        url: "https://teamrevolt.org",
+        description: "Help is subjective and hard sometimes, below is what I can help you with..",
+        image: {
+           url: "https://i.imgur.com/j6YMTdL.png"
+        },
+        fields: [{
+            name: "Why is no one in voice?",
+            value: "Well ah, voice in discord actually sucks, join our [TeamSpeak](ts3server://ts3.teamrevolt.org)."
+          },
+          {
+            name: "Lost your KoTH profile?",
+            value: "Use !profile to learn more about this."
+          },
+          {
+            name: "Servers",
+            value: "We run a lot, vist the forum they can be found there. I'm Lazy after all."
+          }
+        ],
+        timestamp: new Date(),
+        footer: {
+          icon_url: bot.user.avatarURL,
+          text: "©LazyBot, The Revolt Slave"
+        }
+      }
+    });
 
-      message.channel.sendEmbed(embed);
   }
+
+  if (command === 'bans'){
+
+      // Gets the ban counts from battlemetrics
+      axios.get('/bans')
+        .then(function (response) {
+          console.log(response.data.meta);
+          var active = JSON.stringify(response.data.meta.active);
+          var expired = JSON.stringify(response.data.meta.expired);
+          var total = JSON.stringify(response.data.meta.total);
+
+          let embed = new Discord.RichEmbed()
+              .setAuthor("Ban Count")
+              .setDescription("Alright, I'm lazy I can't imagine you are much better.... Here are the numbers")
+              .setColor("#3f542f")
+              .addField("Active", active)
+              .addField("Expired", expired)
+              .addField("Total", total);
+
+          message.channel.sendEmbed(embed);
+
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+  }
+
 
   if (command === 'profile') {
 
@@ -90,7 +215,7 @@ bot.on("message", async message => {
           .addField("First", "Profiles can only presently be done via Discord. Restores can happen at anytime around the clock, and are done by random community members. We don't organise any Restorations.")
           .addField("Second", "Join the WS Discord https://discord.gg/T7QP6cJ");
 
-      message.channel.sendEmbed(embed);
+      message.author.sendEmbed(embed);
   }
 
   if (command === 'Who rocks') {
