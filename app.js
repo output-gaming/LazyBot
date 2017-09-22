c5b1549bf4730cf0f5ab714a3993e0ae199a2ff1
Luke Alford <luke@lukealford.com>
2017-09-22 18:16:35 +1000

    Updating servers in !help

--- app.js
+++ app.js
@@ -178,7 +178,7 @@ bot.on("message", async message => {
           },
           {
             name: "Servers",
-            value: "We run a lot, vist the [forum](https://teamrevolt.org) they can be found there."
+            value: "Check our our Arma 3 Servers<br><br>[ARMAKOTH.COM - King Of The Hill - AU #1 - teamrevolt.org](https://cdn.battlemetrics.com/b/horizontal500x80px/238223.png?foreground=%23EEEEEE&background=%23222222&lines=%23333333&linkColor=%231185ec&chartColor=%23FF0700)[ARMAKOTH.COM - King Of The Hill - AU #2 LV - teamrevolt.org](https://cdn.battlemetrics.com/b/horizontal500x80px/238225.png?foreground=%23EEEEEE&background=%23222222&lines=%23333333&linkColor=%231185ec&chartColor=%23FF0700)[ARMAKOTH.COM - King Of The Hill - AU#3 INF HC - teamrevolt.org](https://cdn.battlemetrics.com/b/horizontal500x80px/238222.png?foreground=%23EEEEEE&background=%23222222&lines=%23333333&linkColor=%231185ec&chartColor=%23FF0700)"
           }
         ],
         timestamp: new Date(),
